a5e7d8435f807f2f74b1c8608ab148d12db69c2d
Jesus Mota <mota@jpcapps.com>
2017-09-04 10:07:50 -0400

    added video

--- build/js/mainController.js
+++ build/js/mainController.js
@@ -39,7 +39,7 @@ function logout()
 {
   $.ajax({
       type: 'POST',
-      url: '/logout',
+      url: URL_BASE+'/logout',
       contentType:'application/json',
       dataType: 'json',
       success: function (data)
@@ -48,6 +48,67 @@ function logout()
           {
             localStorage.removeItem("user");
             location.reload();
+          }
+      }
+    });
+}
+
+
+function changeVideo()
+{
+  $.confirm({
+  title: 'Video!',
+  content:  '<form action="" class="formName">' +
+  '<div class="form-group">' +
+  '<label>Introduzca El Iframe dado por Facebook</label>' +
+  '<textarea id="iframe" type="text" class="name form-control" required />' +
+  '</div>' +
+  '</form>',
+  buttons: {
+      formSubmit: {
+          text: 'Enviar',
+          btnClass: 'btn-blue',
+          action: function () {
+              var iframe = this.$content.find('#iframe').val();
+              sendIframeToServer(iframe);
+          }
+      },
+      cancel: function () {
+          //close
+      },
+  },
+  onContentReady: function () {
+      // bind to events
+      var jc = this;
+      this.$content.find('form').on('submit', function (e) {
+          // if the user submits the form by pressing enter in the field.
+          e.preventDefault();
+          jc.$$formSubmit.trigger('click'); // reference the button and click it
+      });
+  }
+});
+}
+
+
+function sendIframeToServer(iframe)
+{
+  var dataToSend = {};
+  dataToSend.iframe = iframe;
+  $.ajax({
+      type: 'POST',
+      url: URL_BASE+'/video/create',
+      contentType:'application/json',
+      dataType: 'json',
+      data: JSON.stringify(dataToSend),
+      success: function (data)
+      {
+          if (data.success)
+          {
+              showToast("success","video subido satisfactoriamente");
+          }
+          else
+          {
+              showToast("error","error al subir el video");
           }
       }
     });
